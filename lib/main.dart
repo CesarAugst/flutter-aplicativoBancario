e1632862de76c72c80bd2293729649aa97713a72
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-09 14:11:44 -0300

    3-1_Aplicativo completamente segmentado
    Cada particularidade do aplicativo esta dividido em classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencias(),
-          appBar: AppBar(
-            title: Text('Transferencias'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-          ),
-        ),
+void main() => runApp(ByteBank());
+
+class ByteBank extends StatelessWidget {
+  const ByteBank({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
     );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando transferêcias')),
+      body: Text('teste'),
+    );
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   const ListaTransferencias({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTranferencia(Transferencia(100.0, 1000)),
-        ItemTranferencia(Transferencia(200.0, 1001)),
-        ItemTranferencia(Transferencia(300.0, 2000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferencias'),
+      ),
+      body: Column(
+        children: [
+          ItemTranferencia(Transferencia(100.0, 1000)),
+          ItemTranferencia(Transferencia(200.0, 1001)),
+          ItemTranferencia(Transferencia(300.0, 2000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
