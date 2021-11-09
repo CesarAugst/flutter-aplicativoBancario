2bfb17da6bb3494a67aa3b952d6f5a8e3d3009ac
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-09 13:54:43 -0300

    2-6_refatorando
    Criados novas classes para dar maior significado as transações e evitar trabalhar diretamente com os tipos primitivos

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
+          body: ListaTransferencias(),
           appBar: AppBar(
             title: Text('Transferencias'),
           ),
@@ -30,3 +13,42 @@ void main() => runApp(
         ),
       ),
     );
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTranferencia(Transferencia(100.0, 1000)),
+        ItemTranferencia(Transferencia(200.0, 1001)),
+        ItemTranferencia(Transferencia(300.0, 2000)),
+      ],
+    );
+  }
+}
+
+class ItemTranferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTranferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
